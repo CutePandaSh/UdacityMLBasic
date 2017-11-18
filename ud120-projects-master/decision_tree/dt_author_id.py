fb2cfdaa45a70ea3847ea62344e3c06d50e2a4d2
Dennis <9861649@qq.com>
2017-11-18 23:13:41 +0800

    Finish Decision Tree

--- ud120-projects-master/decision_tree/dt_author_id.py
+++ ud120-projects-master/decision_tree/dt_author_id.py
@@ -10,6 +10,8 @@
     
 import sys
 from time import time
+from sklearn.tree import DecisionTreeClassifier as DTC
+from sklearn.metrics import accuracy_score
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -18,8 +20,22 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+print len(features_train[0])
+
+from sklearn.tree import DecisionTreeClassifier as DTC
+clf = DTC(min_samples_split=40)
+print "start fitting...."
+t0 = time()
+clf.fit(features_train, labels_train)
+print 'training time: ',time()-t0
+
+print 'start predicting...'
+t0 = time()
+pred = clf.predict(features_test)
+print 'predict time: ', time()-t0
 
 
+print accuracy_score(pred, labels_test)
 
 
 #########################################################
