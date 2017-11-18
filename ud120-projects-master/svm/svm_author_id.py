ba28d1bb8fabf20d6b6e8e8fed3d9204634fe8fa
Dennis Wang <9861649@qq.com>
2017-11-18 12:00:51 +0800

    no message

--- ud120-projects-master/svm/svm_author_id.py
+++ ud120-projects-master/svm/svm_author_id.py
@@ -26,12 +26,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
-
-
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+# prams = [10.0, 100.0, 1000.0, 10000.0]
 from sklearn.svm import SVC
-clf = SVC(kernal = "linear")
+# for c in prams:
+clf = SVC(kernel = "rbf", C=10000.)
+print "start fitting...."
+t0 = time()
 clf.fit(features_train, labels_train)
+print 'training time: ',time()-t0
+
+print 'start predicting...'
+t0 = time()
 pred = clf.predict(features_test)
+print 'predict time: ', time()-t0
 
 from sklearn.metrics import accuracy_score
 print accuracy_score(pred, labels_test)
